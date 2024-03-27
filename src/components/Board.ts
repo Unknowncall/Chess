1bf016f991617c2b81588fdad6a1f92251aa7475
Zach Harvey <zharvey3131@gmail.com>
2024-03-27 00:42:32 -0500

    Fixed a type

--- src/components/Board.ts
+++ src/components/Board.ts
@@ -4,12 +4,11 @@ export type BoardRow = BoardSlot[]
 export type BoardSlot = { type: PieceType | '', color: PieceColor | '' }
 
 export type Moves = Position[]
-export type Piece = string
 export type Turn = 'w' | 'b'
 export type PieceType = 'p' | 'n' | 'b' | 'r' | 'q' | 'k'
 export type PieceColor = 'w' | 'b'
 
-export const findByPiece = (board: Board, piece: Piece): Position[] => {
+export const findByPiece = (board: Board, piece: PieceType): Position[] => {
 	const positions = [] as Position[]
 	for (let x = 0; x < 8; x++) {
 		for (let y = 0; y < 8; y++) {
